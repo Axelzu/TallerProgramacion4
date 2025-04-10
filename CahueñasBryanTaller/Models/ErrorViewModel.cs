4199cb615777462f627a3bf8c278dad7d13aa93d
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-04-10 18:45:00 -0500

    Agregar archivos de proyecto.

---
+++ CahueñasBryanTaller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CahueñasBryanTaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
