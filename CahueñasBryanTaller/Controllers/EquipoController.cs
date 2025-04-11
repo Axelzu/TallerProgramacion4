06f5b9f375ecd341207ceaef67c659148f8e1b33
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-04-10 20:40:54 -0500

    segundo commit

--- CahueñasBryanTaller/Controllers/EquipoController.cs
+++ CahueñasBryanTaller/Controllers/EquipoController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CahueñasBryanTaller.Models;
+using CahueñasBryanTaller.Respositorio;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CahueñasBryanTaller.Controllers
 {
     public class EquipoController : Controller
     {
-        public ActionResult List()
+        public ActionResult View()
         {
             return View();
+        }
+        public ActionResult List()
+        {
+            EquipoRespository respository = new EquipoRespository();
+            var equipos = respository.DevuelveListadoEquipo();
+
+            equipos = equipos.OrderBy(item => item.PartidosGanados);
+           
+            return View(equipos);
         }
     }
 }
---
+++ CahueñasBryanTaller/Respositorio/EquipoRespository.cs
@@ -0,0 +1,36 @@
+﻿using CahueñasBryanTaller.Models;
+
+namespace CahueñasBryanTaller.Respositorio
+{
+    public class EquipoRespository
+    {
+        public IEnumerable<Equipo> DevuelveListadoEquipo()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "Liga de Quito",
+                PartidosJugados = 10,
+                PartidosGanados = 10,
+                PartidosEmpatados = 10,
+                PartidosPerdidos = 10,
+
+            };
+            Equipo barcelona = new Equipo
+            {
+                Id = 2,
+                Nombre = "Barcelona",
+                PartidosJugados = 10,
+                PartidosGanados = 9,
+                PartidosEmpatados = 5,
+                PartidosPerdidos = 10,
+
+            };
+            equipos.Add(ldu);
+            equipos.Add(barcelona);
+
+            return equipos;
+        }
+    }
+} 
