41e103226142363776072603d9db4fc2b25d873d
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-04-10 19:38:32 -0500

    primer commit

---
+++ CahueñasBryanTaller/Controllers/EquipoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CahueñasBryanTaller.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
